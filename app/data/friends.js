dde33fdb3e6cd49ac58f326f0045bdb32b6a7b92
MAwais <mawais54013@outlook.com>
2018-09-08 14:26:39 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,143 @@
+// Questions: 
+// 1) If you know something is wrong you would speak up to protest?
+// 2) You find it easy to stay calm in difficult situations
+// 3) You are more of a leader than a follower
+// 4) You'll do anything to get more powerful including betraying those close to you?
+// 5) You think everyone's views should be respected?
+// 6) It is often difficult for you to know how others feel?
+// 7) Strategic planning wins over brute force
+// 8) Full scale battle over close combat fighting 
+// 9) In every fight there will be "One will stand and One will fall"
+// 10) You would be willing to betray your team to join the enemy if your leader does not like you
+
+var friendsArray = 
+[
+    {
+        name: "Megatron",
+        photo: "https://orig00.deviantart.net/7df9/f/2012/160/c/5/transformers_prime__megatron_by_gravedigger68-d52uq2j.jpg",
+        scores: [
+            5, 
+            3,
+            5,
+            5,
+            1,
+            1,
+            1,
+            3,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Optimus Prime",
+        photo: "https://i.ytimg.com/vi/jgMaDAEoEOk/maxresdefault.jpg",
+        scores: [
+            5,
+            4,
+            5,
+            2,
+            5,
+            2,
+            5,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Bumble Bee",
+        photo: "https://i.ytimg.com/vi/buo1bfVEJ18/maxresdefault.jpg",
+        scores: [
+            4,
+            5,
+            4,
+            3,
+            5,
+            3,
+            4,
+            3,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Knock Out",
+        photo: "https://orig00.deviantart.net/fd9a/f/2011/326/2/a/knockout_tfp_by_transformersmix-d4gxvzp.png",
+        scores: [
+            3,
+            2,
+            4,
+            5,
+            1,
+            4,
+            1,
+            5,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Predaking",
+        photo: "https://vignette.wikia.nocookie.net/transformersprime/images/8/8c/Dragon_096.PNG/revision/latest?cb=20130723163604",
+        scores: [
+            5,
+            2,
+            5,
+            2,
+            1,
+            3,
+            2,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Ratchet",
+        photo: "https://vignette.wikia.nocookie.net/transformersprime/images/5/57/Ratchet_smile.jpeg/revision/latest?cb=20140305001904",
+        scores: [
+            5,
+            3,
+            2,
+            2,
+            4,
+            1,
+            5,
+            3,
+            2,
+            4
+        ]
+    },
+    {
+        name: "Shockwave",
+        photo: "https://i.ytimg.com/vi/0NHIlU4l02E/maxresdefault.jpg",
+        scores: [
+            3,
+            5,
+            2,
+            1,
+            1,
+            5,
+            3,
+            3,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Soundwave",
+        photo: "https://i.ytimg.com/vi/XHmhGiB5W6A/maxresdefault.jpg",
+        scores: [
+            1,
+            5,
+            1,
+            2,
+            1,
+            1,
+            5,
+            3,
+            2,
+            1
+        ]
+    },
+]
